dc8b5efd4e09045142b68eac75512c03d1d032ed
Subaraki <robbeenaxel@hotmail.com>
2022-10-05 22:28:08 +0200

    modify name to fit vanilla entity naming. make one common entity. merge baby owl class with owl class. switch logic on isBaby() rather then in another class. remove duplicate code by making only one entity class. fix persitency in use of camelCase field - and String names. play shake sound in the apropriate method (ageBoundaryReached) rather then overriding the method that calls this exact method.

--- src/main/java/net/brogli/broglisowls/entity/custom/Owl.java
+++ src/main/java/net/brogli/broglisowls/entity/custom/Owl.java
@@ -11,7 +11,8 @@ import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.util.Mth;
-import net.minecraft.world.DifficultyInstance;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.*;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
@@ -28,7 +29,6 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.ServerLevelAccessor;
 import net.minecraft.world.level.block.LeavesBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.gameevent.GameEvent;
@@ -45,9 +45,9 @@ import software.bernie.geckolib3.core.manager.AnimationFactory;
 
 import java.util.function.Predicate;
 
-public class EntityOwl extends Animal implements IAnimatable {
+public class Owl extends Animal implements IAnimatable {
 
-    private AnimationFactory factory = new AnimationFactory(this);
+    private final AnimationFactory factory = new AnimationFactory(this);
 
     public int eggTime = this.random.nextInt(6000) + 6000;
 
@@ -69,7 +69,7 @@ public class EntityOwl extends Animal implements IAnimatable {
     private boolean isShaking;
 
 
-    public EntityOwl(EntityType<? extends Animal> entityType, Level level) {
+    public Owl(EntityType<? extends Animal> entityType, Level level) {
         super(entityType, level);
         this.moveControl = new FlyingMoveControl(this, 10, false);
         this.setPathfindingMalus(BlockPathTypes.DANGER_FIRE, -1.0F);
@@ -79,9 +79,12 @@ public class EntityOwl extends Animal implements IAnimatable {
     public static AttributeSupplier setAttributes() {
         return Animal.createMobAttributes()
                 .add(Attributes.MAX_HEALTH, 10.0D)
-                .add(Attributes.FLYING_SPEED, (double)0.5F)
-                .add(Attributes.MOVEMENT_SPEED, (double)0.2F)
+                .add(Attributes.FLYING_SPEED, (double) 0.5F)
+                //TODO baby = 0.4f
+                .add(Attributes.MOVEMENT_SPEED, (double) 0.2F)
+                //TODO baby = 0.17f
                 .add(Attributes.ATTACK_DAMAGE, 2.0D)
+                //TODO baby os 1.0
                 .add(Attributes.FOLLOW_RANGE, 32.0D).build();
     }
 
@@ -90,32 +93,34 @@ public class EntityOwl extends Animal implements IAnimatable {
         this.goalSelector.addGoal(0, new FloatGoal(this));
         this.goalSelector.addGoal(1, new BreedGoal(this, 1.0D));
         this.goalSelector.addGoal(2, new TemptGoal(this, 1.25D, Ingredient.of(Items.RABBIT), false));
+        this.goalSelector.addGoal(2, new FollowParentGoal(this, 1.25D));
         this.goalSelector.addGoal(5, new MeleeAttackGoal(this, 1.0D, true));
         this.goalSelector.addGoal(5, new LookAtPlayerGoal(this, Player.class, 32.0F));
         this.targetSelector.addGoal(10, new NearestAttackableTargetGoal<>(this, Animal.class, false, PREY_SELECTOR));
         this.goalSelector.addGoal(15, new RandomLookAroundGoal(this));
-        this.goalSelector.addGoal(30, new EntityOwl.OwlWanderGoal(this, 1.0D));
+        this.goalSelector.addGoal(30, new OwlWanderGoal(this, 1.0D));
     }
 
-    protected PathNavigation createNavigation(Level p_29417_) {
-        FlyingPathNavigation flyingpathnavigation = new FlyingPathNavigation(this, p_29417_);
+    protected PathNavigation createNavigation(Level level) {
+        FlyingPathNavigation flyingpathnavigation = new FlyingPathNavigation(this, level);
         flyingpathnavigation.setCanOpenDoors(false);
         flyingpathnavigation.setCanFloat(true);
         flyingpathnavigation.setCanPassDoors(true);
         return flyingpathnavigation;
     }
 
-    public void readAdditionalSaveData(CompoundTag tag) {
-        super.readAdditionalSaveData(tag);
-        if (tag.contains("EggLayTime")) {
-            this.eggTime = tag.getInt("EggLayTime");
-        }
-
-    }
-
     public void addAdditionalSaveData(CompoundTag tag) {
         super.addAdditionalSaveData(tag);
-        tag.putInt("EggLayTime", this.eggTime);
+        tag.putInt("age", this.age);
+        tag.putInt("eggLayTime", this.eggTime);
+    }
+
+    public void readAdditionalSaveData(CompoundTag tag) {
+        super.readAdditionalSaveData(tag);
+        this.setAge(tag.getInt("age"));
+        if (tag.contains("eggLayTime")) {
+            this.eggTime = tag.getInt("eggLayTime");
+        }
     }
 
     public void aiStep() {
@@ -124,32 +129,60 @@ public class EntityOwl extends Animal implements IAnimatable {
             this.isShaking = true;
             this.shakeAnim = 0.0F;
             this.shakeAnimO = 0.0F;
-            this.level.broadcastEntityEvent(this, (byte)8);
+            this.level.broadcastEntityEvent(this, (byte) 8);
         }
 
-        if (this.level.random.nextInt(400) == 0) {
-            this.playSound(BroglisOwlsSounds.OWL_AMBIENT_1.get(), 1.5F, (this.random.nextFloat() - this.random.nextFloat()) * 0.2F + 1.0F);
+        if (this.level.random.nextInt(500) == 0) {
+            this.playSound(this.isBaby() ? BroglisOwlsSounds.BABY_OWL_AMBIENT_1.get() : BroglisOwlsSounds.OWL_AMBIENT_1.get(), this.isBaby() ? 0.3F : 1.0F, (this.random.nextFloat() - this.random.nextFloat()) * 0.2F + 1.0F);
         }
 
-        if (!this.level.isClientSide && this.isAlive() && --this.eggTime <= 0) {
+        if (!this.level.isClientSide && !this.isBaby() && this.isAlive() && --this.eggTime <= 0) {
             this.playSound(SoundEvents.CHICKEN_EGG, 1.0F, (this.random.nextFloat() - this.random.nextFloat()) * 0.2F + 1.0F);
             this.spawnAtLocation(BroglisOwlsItems.ITEM_OWL_EGG.get());
             this.gameEvent(GameEvent.ENTITY_PLACE);
             this.eggTime = this.random.nextInt(6000) + 6000;
         }
-
         super.aiStep();
         this.calculateFlapping();
+
+    }
+
+    @Override
+    protected void ageBoundaryReached() {
+        super.ageBoundaryReached();
+        this.playSound(SoundEvents.WOLF_SHAKE, 0.15F, 1.0F);
+    }
+
+    @Override
+    public InteractionResult mobInteract(Player player, InteractionHand hand) {
+        ItemStack itemstack = player.getItemInHand(hand);
+        if (this.isFood(itemstack) && this.isBaby()) {
+            this.feed(player, itemstack);
+            return InteractionResult.sidedSuccess(this.level.isClientSide);
+        }
+        return super.mobInteract(player, hand);
+    }
+
+
+    private void feed(Player player, ItemStack stack) {
+        if (!player.getAbilities().instabuild)
+            stack.shrink(1);
+        this.ageUp(AgeableMob.getSpeedUpSecondsWhenFeeding(-this.getAge()));
+        this.level.addParticle(ParticleTypes.HAPPY_VILLAGER, this.getRandomX(1.0D), this.getRandomY() + 0.5D, this.getRandomZ(1.0D), 0.0D, 0.0D, 0.0D);
     }
 
     public boolean shouldDropExperience() {
         return false;
     }
 
+    public boolean canBreed() {
+        return false;
+    }
+
     private void calculateFlapping() {
         this.oFlap = this.flap;
         this.oFlapSpeed = this.flapSpeed;
-        this.flapSpeed += (float)(!this.onGround && !this.isPassenger() ? 4 : -1) * 0.3F;
+        this.flapSpeed += (float) (!this.onGround && !this.isPassenger() ? 4 : -1) * 0.3F;
         this.flapSpeed = Mth.clamp(this.flapSpeed, 0.0F, 1.0F);
         if (!this.onGround && this.flapping < 1.0F) {
             this.flapping = 1.0F;
@@ -169,7 +202,7 @@ public class EntityOwl extends Animal implements IAnimatable {
     }
 
     protected void onFlap() {
-        this.playSound(SoundEvents.PARROT_FLY, 0.25F, 1.0F);
+        this.playSound(SoundEvents.PARROT_FLY, 0.15F, 1.0F);
         this.nextFlap = this.flyDist + this.flapSpeed / 2.0F;
     }
 
@@ -184,7 +217,7 @@ public class EntityOwl extends Animal implements IAnimatable {
             if (this.isInWaterRainOrBubble()) {
                 this.isWet = true;
                 if (this.isShaking && !this.level.isClientSide) {
-                    this.level.broadcastEntityEvent(this, (byte)56);
+                    this.level.broadcastEntityEvent(this, (byte) 56);
                     this.cancelShake();
                 }
             } else if ((this.isWet || this.isShaking) && this.isShaking) {
@@ -201,14 +234,14 @@ public class EntityOwl extends Animal implements IAnimatable {
                 }
 
                 if (this.shakeAnim > 0.4F) {
-                    float f = (float)this.getY();
-                    int i = (int)(Mth.sin((this.shakeAnim - 0.4F) * (float)Math.PI) * 7.0F);
+                    float f = (float) this.getY();
+                    int i = (int) (Mth.sin((this.shakeAnim - 0.4F) * (float) Math.PI) * 7.0F);
                     Vec3 vec3 = this.getDeltaMovement();
 
-                    for(int j = 0; j < i; ++j) {
+                    for (int j = 0; j < i; ++j) {
                         float f1 = (this.random.nextFloat() * 2.0F - 1.0F) * this.getBbWidth() * 0.5F;
                         float f2 = (this.random.nextFloat() * 2.0F - 1.0F) * this.getBbWidth() * 0.5F;
-                        this.level.addParticle(ParticleTypes.SPLASH, this.getX() + (double)f1, (double)(f + 0.8F), this.getZ() + (double)f2, vec3.x, vec3.y, vec3.z);
+                        this.level.addParticle(ParticleTypes.SPLASH, this.getX() + (double) f1, (double) (f + 0.8F), this.getZ() + (double) f2, vec3.x, vec3.y, vec3.z);
                     }
                 }
             }
@@ -235,16 +268,16 @@ public class EntityOwl extends Animal implements IAnimatable {
         this.shakeAnimO = 0.0F;
     }
 
-    public void die(DamageSource p_30384_) {
+    public void die(DamageSource source) {
         this.isWet = false;
         this.isShaking = false;
         this.shakeAnimO = 0.0F;
         this.shakeAnim = 0.0F;
-        super.die(p_30384_);
+        super.die(source);
     }
 
     public boolean doHurtTarget(Entity entity) {
-        boolean flag = entity.hurt(DamageSource.mobAttack(this), (float)((int)this.getAttributeValue(Attributes.ATTACK_DAMAGE)));
+        boolean flag = entity.hurt(DamageSource.mobAttack(this), (float) ((int) this.getAttributeValue(Attributes.ATTACK_DAMAGE)));
         if (flag) {
             this.doEnchantDamageEffects(this, entity);
         }
@@ -268,25 +301,23 @@ public class EntityOwl extends Animal implements IAnimatable {
     @Nullable
     @Override
     public AgeableMob getBreedOffspring(ServerLevel serverLevel, AgeableMob mob) {
-        EntityBabyOwl baby = BroglisOwlsEntityTypes.ENTITY_BABY_OWL.get().create(serverLevel);
-        return baby;
+        return BroglisOwlsEntityTypes.OWL_ENTITY_TYPE.get().create(serverLevel);
     }
 
     private <E extends IAnimatable> PlayState predicate(AnimationEvent<E> event) {
         if (this.isShaking) {
-            System.out.println("Owl is shaking");
-            event.getController().setAnimation(new AnimationBuilder().addAnimation("animation.entity_owl.ruffle", false));
+            event.getController().setAnimation(new AnimationBuilder().addAnimation("animation.entity_baby_owl.ruffle", false));
             return PlayState.CONTINUE;
         }
         if (this.onGround && event.isMoving()) {
-            event.getController().setAnimation(new AnimationBuilder().addAnimation("animation.entity_owl.walk", true));
+            event.getController().setAnimation(new AnimationBuilder().addAnimation("animation.entity_baby_owl.walk", true));
             return PlayState.CONTINUE;
         }
         if (this.isFlying()) {
-            event.getController().setAnimation(new AnimationBuilder().addAnimation("animation.entity_owl.flight", true));
+            event.getController().setAnimation(new AnimationBuilder().addAnimation("animation.entity_baby_owl.flight", true));
             return PlayState.CONTINUE;
         }
-        event.getController().setAnimation(new AnimationBuilder().addAnimation("animation.entity_owl.idle", true));
+        event.getController().setAnimation(new AnimationBuilder().addAnimation("animation.entity_baby_owl.idle", true));
         return PlayState.CONTINUE;
     }
 
@@ -300,18 +331,9 @@ public class EntityOwl extends Animal implements IAnimatable {
         return this.factory;
     }
 
-    @javax.annotation.Nullable
-    public SpawnGroupData finalizeSpawn(ServerLevelAccessor levelAccessor, DifficultyInstance instance, MobSpawnType spawnType, @javax.annotation.Nullable SpawnGroupData groupData, @javax.annotation.Nullable CompoundTag tag) {
-        if (groupData == null) {
-            groupData = new AgeableMob.AgeableMobGroupData(false);
-        }
-
-        return super.finalizeSpawn(levelAccessor, instance, spawnType, groupData, tag);
-    }
-
     static class OwlWanderGoal extends WaterAvoidingRandomFlyingGoal {
-        public OwlWanderGoal(PathfinderMob p_186224_, double p_186225_) {
-            super(p_186224_, p_186225_);
+        public OwlWanderGoal(PathfinderMob pathFinder, double distance) {
+            super(pathFinder, distance);
         }
 
         @javax.annotation.Nullable
@@ -334,7 +356,7 @@ public class EntityOwl extends Animal implements IAnimatable {
             BlockPos.MutableBlockPos blockpos$mutableblockpos = new BlockPos.MutableBlockPos();
             BlockPos.MutableBlockPos blockpos$mutableblockpos1 = new BlockPos.MutableBlockPos();
 
-            for(BlockPos blockpos1 : BlockPos.betweenClosed(Mth.floor(this.mob.getX() - 3.0D), Mth.floor(this.mob.getY() - 6.0D), Mth.floor(this.mob.getZ() - 3.0D), Mth.floor(this.mob.getX() + 3.0D), Mth.floor(this.mob.getY() + 6.0D), Mth.floor(this.mob.getZ() + 3.0D))) {
+            for (BlockPos blockpos1 : BlockPos.betweenClosed(Mth.floor(this.mob.getX() - 3.0D), Mth.floor(this.mob.getY() - 6.0D), Mth.floor(this.mob.getZ() - 3.0D), Mth.floor(this.mob.getX() + 3.0D), Mth.floor(this.mob.getY() + 6.0D), Mth.floor(this.mob.getZ() + 3.0D))) {
                 if (!blockpos.equals(blockpos1)) {
                     BlockState blockstate = this.mob.level.getBlockState(blockpos$mutableblockpos1.setWithOffset(blockpos1, Direction.DOWN));
                     boolean flag = blockstate.getBlock() instanceof LeavesBlock || blockstate.is(BlockTags.LOGS);
