9be145898b100f46c545ec7fabd981b90cc71b73
Subaraki <robbeenaxel@hotmail.com>
2022-10-05 22:28:28 +0200

    make one common entity.

--- src/main/java/net/brogli/broglisowls/event/ModEventClientBusEvents.java
+++ src/main/java/net/brogli/broglisowls/event/ModEventClientBusEvents.java
@@ -3,13 +3,11 @@ package net.brogli.broglisowls.event;
 
 import net.brogli.broglisowls.BroglisOwls;
 import net.brogli.broglisowls.entity.BroglisOwlsEntityTypes;
-import net.brogli.broglisowls.entity.client.EntityBabyOwlRenderer;
-import net.brogli.broglisowls.entity.client.EntityOwlRenderer;
+import net.brogli.broglisowls.entity.client.OwlRenderer;
 import net.brogli.broglisowls.entity.client.ThrownOwlEggRenderer;
 import net.minecraft.client.renderer.entity.EntityRenderers;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.EntityRenderersEvent;
-import net.minecraftforge.client.event.RegisterParticleProvidersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
@@ -19,19 +17,11 @@ public class ModEventClientBusEvents {
 
     @SubscribeEvent
     public static void clientSetup(final FMLClientSetupEvent event) {
-        EntityRenderers.register(BroglisOwlsEntityTypes.ENTITY_OWL.get(), EntityOwlRenderer::new);
-        EntityRenderers.register(BroglisOwlsEntityTypes.ENTITY_BABY_OWL.get(), EntityBabyOwlRenderer::new);
+        EntityRenderers.register(BroglisOwlsEntityTypes.OWL_ENTITY_TYPE.get(), OwlRenderer::new);
         EntityRenderers.register(BroglisOwlsEntityTypes.THROWN_OWL_EGG.get(), ThrownOwlEggRenderer::new);
     }
 
     @SubscribeEvent
     public static void registerRenderers(final EntityRenderersEvent.RegisterRenderers event) {
     }
-
-    @SubscribeEvent
-    public static void registerParticleFactories(final RegisterParticleProvidersEvent event) {
-//        Minecraft.getInstance().particleEngine.register(BroglisOwlsParticles.FEATHER_PARTICLES.get(),
-//                FeatherParticles.Provider::new);
-    }
-
 }
