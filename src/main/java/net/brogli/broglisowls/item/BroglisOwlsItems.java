62ef1d9892637f1acd56b928e0c33a622a45b02e
Subaraki <robbeenaxel@hotmail.com>
2022-10-05 22:30:57 +0200

    move properties to item class for a shorter registry call.

--- src/main/java/net/brogli/broglisowls/item/BroglisOwlsItems.java
+++ src/main/java/net/brogli/broglisowls/item/BroglisOwlsItems.java
@@ -2,7 +2,6 @@ package net.brogli.broglisowls.item;
 
 import net.brogli.broglisowls.BroglisOwls;
 import net.brogli.broglisowls.item.custom.ItemOwlEgg;
-import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -13,8 +12,7 @@ public class BroglisOwlsItems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, BroglisOwls.MOD_ID);
 
-    public static final RegistryObject<ItemOwlEgg> ITEM_OWL_EGG = ITEMS.register("item_owl_egg",
-            () -> new ItemOwlEgg(new Item.Properties().stacksTo(16).tab(CreativeModeTab.TAB_MATERIALS)));
+    public static final RegistryObject<ItemOwlEgg> ITEM_OWL_EGG = ITEMS.register("item_owl_egg", ItemOwlEgg::new);
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
